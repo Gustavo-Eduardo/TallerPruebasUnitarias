faab6ca9e24b5ccb97bd71f9466779f6ced25eca
Gustavo-Eduardo <guedlope@espol.edu.ec>
2021-08-23 10:28:53 -0500

    3 casos de prueba

--- Employee/src/tests/EmployeeTest.java
+++ Employee/src/tests/EmployeeTest.java
@@ -1,14 +1,35 @@
 package tests;
 
-import static org.junit.Assert.*;
-
 import org.junit.Test;
 
-public class EmployeeTest {
+import junit.framework.TestCase;
+import tallerpruebas.Employee;
+import tallerpruebas.EmployeeType;
+
+public class EmployeeTest extends TestCase {
+	
+	private Employee employee;
+	
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testWorkerCS() {
+		employee = new Employee(360.0F , "USD", 0.05F, EmployeeType.Worker);
+		float cs = employee.cs();		
+		assertEquals(360.0F, cs);
+	}	
+	
+	@Test
+	public void testWorkerYearBonus() {
+		employee = new Employee(360.0F , "USD", 0.05F, EmployeeType.Worker);
+		float yearBonus = employee.CalculateYearBonus();
+		assertEquals(386.0F, yearBonus);
+	}
+	
+	@Test
+	public void testNotUSDCurrency() {
+		employee = new Employee(360.0F , "EUR", 0.05F, EmployeeType.Worker);
+		float cs = employee.cs();
+		assertEquals(342.0F, cs);
 	}
 
 }
