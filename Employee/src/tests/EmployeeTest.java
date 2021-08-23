2ed6996075d2396bd220b08270c18b0a88cce990
Jorge15042001 <adrivulgarin@hotmail.com>
2021-08-23 10:35:05 -0500

    test2

--- Employee/src/tests/EmployeeTest.java
+++ Employee/src/tests/EmployeeTest.java
@@ -4,11 +4,23 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import tallerpruebas.Employee;
+import tallerpruebas.EmployeeType;
+
 public class EmployeeTest {
 
 	@Test
 	public void test() {
 		fail("Not yet implemented");
 	}
+    @Test
+    public void test2()
+    {
+        Employee e = new Employee(450,"EUR",10,EmployeeType.Worker);
+        final float paga = e.cs();
+        assertTrue(450== paga);
+    }
+
+    
 
 }
