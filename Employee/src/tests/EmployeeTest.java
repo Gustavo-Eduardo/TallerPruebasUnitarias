c2125c135761188dae4b51a442223b619a367c5f
Gustavo-Eduardo <guedlope@espol.edu.ec>
2021-08-23 11:27:30 -0500

    mas casos de prueba

--- Employee/src/tests/EmployeeTest.java
+++ Employee/src/tests/EmployeeTest.java
@@ -16,7 +16,21 @@ public class EmployeeTest extends TestCase {
 		employee = new Employee(360.0F , "USD", 0.05F, EmployeeType.Worker);
 		float cs = employee.cs();		
 		assertEquals(360.0F, cs);
-	}	
+	}
+	
+	@Test
+	public void testSupervisorCS() {
+		employee = new Employee(360.0F , "USD", 0.05F, EmployeeType.Supervisor);
+		float cs = employee.cs();		
+		assertEquals(360.0175F, cs);
+	}
+	
+	@Test
+	public void testManagerCS() {
+		employee = new Employee(360.0F , "USD", 0.05F, EmployeeType.Manager);
+		float cs = employee.cs();		
+		assertEquals(360.035F, cs);
+	}
 	
 	@Test
 	public void testWorkerYearBonus() {
@@ -24,18 +38,12 @@ public class EmployeeTest extends TestCase {
 		float yearBonus = employee.CalculateYearBonus();
 		assertEquals(386.0F, yearBonus);
 	}
-	
-	@Test
-	public void testNotUSDCurrency() {
-		employee = new Employee(360.0F , "EUR", 0.05F, EmployeeType.Worker);
-		float cs = employee.cs();
-		assertEquals(342.0F, cs);
-	}
+		
 	@Test
 	public void testSupervisorYearBonus() {
         final float salario = 500;
         final float expectedValue = salario + 386/2;
-		employee = new Employee(salario , "USD", 0.05F, EmployeeType.Worker);
+		employee = new Employee(salario , "USD", 0.05F, EmployeeType.Supervisor);
 		float yearBonus = employee.CalculateYearBonus();
 		assertEquals(expectedValue, yearBonus);
 	}
@@ -43,9 +51,23 @@ public class EmployeeTest extends TestCase {
 	public void testManagerYearBonus() {
         final float salario = 800;
         final float expectedValue = salario + 386;
-		employee = new Employee(salario , "USD", 0.05F, EmployeeType.Worker);
+		employee = new Employee(salario , "USD", 0.05F, EmployeeType.Manager);
 		float yearBonus = employee.CalculateYearBonus();
 		assertEquals(expectedValue, yearBonus);
+	}
+	
+	@Test
+	public void testNotUSDCurrencyCS() {
+		employee = new Employee(360.0F , "EUR", 0.05F, EmployeeType.Worker);
+		float cs = employee.cs();
+		assertEquals(342.0F, cs);
+	}
+	
+	@Test
+	public void testNotUSDCurrencyYearBonus() {
+		employee = new Employee(360.0F , "EUR", 0.05F, EmployeeType.Supervisor);
+		float yearBonus = employee.CalculateYearBonus();
+		assertEquals(535.0F, yearBonus);
 	}
 	
 
